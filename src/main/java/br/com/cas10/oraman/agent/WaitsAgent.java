3e9ef5a92359c810a9501618eb245a5261128afa
Eduardo Marubayashi <eduardo.marubayashi@touchtec.com.br>
2018-09-11 01:23:25 -0300

    WaitsAgent: check v$system_event and v$sys_time_model

--- src/main/java/br/com/cas10/oraman/agent/WaitsAgent.java
+++ src/main/java/br/com/cas10/oraman/agent/WaitsAgent.java
@@ -1,19 +1,26 @@
 package br.com.cas10.oraman.agent;
 
+import static br.com.cas10.oraman.oracle.OracleObject.V_SYSTEM_EVENT;
+import static br.com.cas10.oraman.oracle.OracleObject.V_SYS_TIME_MODEL;
 import static java.util.concurrent.TimeUnit.HOURS;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+import br.com.cas10.oraman.oracle.AccessChecker;
 import br.com.cas10.oraman.oracle.Waits;
 import br.com.cas10.oraman.util.DeltaBuffer;
 import br.com.cas10.oraman.util.Snapshot;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
@@ -21,11 +28,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class WaitsAgent {
 
+  private static final Logger logger = LoggerFactory.getLogger(WaitsAgent.class);
+
   @VisibleForTesting
   static final long SAMPLING_INTERVAL_MILLIS = SECONDS.toMillis(15);
 
   private static final String CPU_CLASS = "CPU";
 
+  @Autowired
+  private AccessChecker accessChecker;
   @Autowired
   private TaskScheduler scheduler;
   @Autowired
@@ -38,7 +49,20 @@ public class WaitsAgent {
     ImmutableList.Builder<String> builder = ImmutableList.builder();
     waitClasses = builder.add(CPU_CLASS).addAll(waits.getWaitClasses()).build();
 
-    scheduler.scheduleAtFixedRate(this::run, SAMPLING_INTERVAL_MILLIS);
+    List<String> notAccessible = new ArrayList<>();
+    if (!accessChecker.isQueryable(V_SYS_TIME_MODEL)) {
+      notAccessible.add(V_SYS_TIME_MODEL.name);
+    }
+    if (!accessChecker.isQueryable(V_SYSTEM_EVENT)) {
+      notAccessible.add(V_SYSTEM_EVENT.name);
+    }
+
+    if (notAccessible.isEmpty()) {
+      scheduler.scheduleAtFixedRate(this::run, SAMPLING_INTERVAL_MILLIS);
+    } else {
+      logger.warn("Not accessible: {}. WaitsAgent will not be started.",
+          Joiner.on(", ").join(notAccessible));
+    }
   }
 
   private final DeltaBuffer<Snapshot<Long>, Snapshot<Double>> snapshots;
--- src/main/java/br/com/cas10/oraman/oracle/OracleObject.java
+++ src/main/java/br/com/cas10/oraman/oracle/OracleObject.java
@@ -2,9 +2,13 @@ package br.com.cas10.oraman.oracle;
 
 public enum OracleObject {
 
-  V_EVENT_NAME("v$event_name");
+  V_EVENT_NAME("v$event_name"),
 
-  final String name;
+  V_SYS_TIME_MODEL("v$sys_time_model"),
+
+  V_SYSTEM_EVENT("v$system_event");
+
+  public final String name;
 
   OracleObject(String name) {
     this.name = name;
