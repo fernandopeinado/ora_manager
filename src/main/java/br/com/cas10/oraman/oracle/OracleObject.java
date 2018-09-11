f9411f42973b25e9e790270fad96abbbc03fda8f
Eduardo Marubayashi <eduardo.marubayashi@touchtec.com.br>
2018-09-11 16:35:06 -0300

    Tablespaces: check dba_data_files and dba_free_space
    Change-Id: Id1e97432c649adeffec63c394ddda61834c425a9

--- src/main/java/br/com/cas10/oraman/oracle/OracleObject.java
+++ src/main/java/br/com/cas10/oraman/oracle/OracleObject.java
@@ -2,6 +2,10 @@ package br.com.cas10.oraman.oracle;
 
 public enum OracleObject {
 
+  DBA_DATA_FILES("dba_data_files"),
+
+  DBA_FREE_SPACE("dba_free_space"),
+
   V_EVENT_NAME("v$event_name"),
 
   V_SQLCOMMAND("v$sqlcommand"),
--- src/main/java/br/com/cas10/oraman/oracle/Tablespaces.java
+++ src/main/java/br/com/cas10/oraman/oracle/Tablespaces.java
@@ -1,16 +1,26 @@
 package br.com.cas10.oraman.oracle;
 
+import static br.com.cas10.oraman.oracle.OracleObject.DBA_DATA_FILES;
+import static br.com.cas10.oraman.oracle.OracleObject.DBA_FREE_SPACE;
+
 import br.com.cas10.oraman.oracle.data.TablespaceUsage;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class Tablespaces {
+
+  private static final Logger logger = LoggerFactory.getLogger(Tablespaces.class);
 
   private static final RowMapper<TablespaceUsage> TBLSPACE_USAGE_ROW_MAPPER = (rs, rownum) -> {
     TablespaceUsage bean = new TablespaceUsage();
@@ -24,15 +34,39 @@ public class Tablespaces {
   private final String tablespaceUsageSql;
 
   @Autowired
+  private AccessChecker accessChecker;
+  @Autowired
+  private JdbcTemplate jdbc;
+
+  private boolean requiredObjectsAccessible;
+
+  @Autowired
   public Tablespaces(SqlFileLoader loader) {
     tablespaceUsageSql = loader.load("tablespaces_usage.sql");
   }
 
-  @Autowired
-  private NamedParameterJdbcTemplate jdbc;
+  @PostConstruct
+  private void init() {
+    List<String> notAccessible = new ArrayList<>();
+    if (!accessChecker.isQueryable(DBA_DATA_FILES)) {
+      notAccessible.add(DBA_DATA_FILES.name);
+    }
+    if (!accessChecker.isQueryable(DBA_FREE_SPACE)) {
+      notAccessible.add(DBA_FREE_SPACE.name);
+    }
+
+    requiredObjectsAccessible = notAccessible.isEmpty();
+
+    if (!notAccessible.isEmpty()) {
+      logger.warn("Not accessible: {}", Joiner.on(", ").join(notAccessible));
+    }
+  }
 
   @Transactional(readOnly = true)
   public List<TablespaceUsage> getUsage() {
-    return jdbc.query(tablespaceUsageSql, ImmutableMap.of(), TBLSPACE_USAGE_ROW_MAPPER);
+    if (!requiredObjectsAccessible) {
+      return ImmutableList.of();
+    }
+    return jdbc.query(tablespaceUsageSql, TBLSPACE_USAGE_ROW_MAPPER);
   }
 }
